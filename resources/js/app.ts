7ca55c8f552f50bec9d41e2220e5b540187bb303
Syofyan Zuhad <mail@syofyanzuhad.dev>
2026-07-31 02:02:32 +0700

    fix: bypass default AppLayout for Share and Demo routes in app.ts

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -11,7 +11,7 @@ createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
     layout: (name) => {
         switch (true) {
-            case name === 'Welcome':
+            case name === 'Welcome' || name === 'Demo' || name.startsWith('Share/'):
                 return null;
             case name.startsWith('auth/'):
                 return AuthLayout;
