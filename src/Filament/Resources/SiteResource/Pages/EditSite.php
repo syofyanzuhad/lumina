cedc343f7011a19ece607ed7011bac5e3a94b158
Syofyan Zuhad <mail@syofyanzuhad.dev>
2026-08-08 16:25:05 +0700

    feat: implement Filament admin panel resource, widgets, and LuminaPlugin

---
+++ src/Filament/Resources/SiteResource/Pages/EditSite.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Lumina\Core\Filament\Resources\SiteResource\Pages;
+
+use Filament\Resources\Pages\EditRecord;
+use Lumina\Core\Filament\Resources\SiteResource;
+
+class EditSite extends EditRecord
+{
+    protected static string $resource = SiteResource::class;
+}
---
+++ src/Filament/Resources/SiteResource/Pages/ListSites.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Lumina\Core\Filament\Resources\SiteResource\Pages;
+
+use Filament\Resources\Pages\ListRecords;
+use Lumina\Core\Filament\Resources\SiteResource;
+
+class ListSites extends ListRecords
+{
+    protected static string $resource = SiteResource::class;
+}
